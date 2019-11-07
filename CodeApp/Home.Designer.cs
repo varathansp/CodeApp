9ffd6cf4a9c8acd8887d47bc8297b8e520f181f6
Varathan <varathansuyambu95@gmail.com>
2019-11-07 13:41:06 +0530

    Add project files.

---
+++ CodeApp/Home.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CodeApp
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFactorial = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFactorial
+            // 
+            this.btnFactorial.Location = new System.Drawing.Point(38, 58);
+            this.btnFactorial.Name = "btnFactorial";
+            this.btnFactorial.Size = new System.Drawing.Size(75, 23);
+            this.btnFactorial.TabIndex = 0;
+            this.btnFactorial.Text = "Factorial";
+            this.btnFactorial.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(158, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(229, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 321);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnFactorial);
+            this.Name = "Home";
+            this.Text = "Code App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFactorial;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
